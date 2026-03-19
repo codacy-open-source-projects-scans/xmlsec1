41dcf34388a3e759881095af213462d8879e3987
Codacy Archive <codacy-archive@local>
2026-03-19 01:22:06 +0000

    snapshot 2026-03-19T01:22:06.964217+00:00

--- apps/unit_tests/xmlsec_unit_tests.h
+++ apps/unit_tests/xmlsec_unit_tests.h
@@ -28,6 +28,11 @@ int  testGroupFinished(void);
 void testStart(const char * name);
 void testFinishedSuccess(void);
 void testFinishedFailure(void);
+#ifdef __GNUC__
+void testLog(const char* fmt, ...) __attribute__ ((format (printf, 1, 2)));
+#else /* __GNUC__ */
+void testLog(const char* fmt, ...);
+#endif /* __GNUC__ */
 
 
 /* tests */
@@ -36,6 +41,8 @@ int test_transform_helpers(void);
 int test_xmlSecX509EscapedStringRead(void);
 int test_xmlSecX509AttrValueStringRead(void);
 int test_xmlSecX509NameRead(void);
+int test_xmltree(void);
+int test_templates(void);
 
 #ifdef __cplusplus
 }
